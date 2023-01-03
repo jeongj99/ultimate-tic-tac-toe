2e5528d6ae89605e9a1adeb765b84435e2e046b1
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-02 21:49:52 -0300

    Create Ultimate component and import Tictactoe component into it

---
+++ src/components/Ultimate.js
@@ -0,0 +1,27 @@
+import Tictactoe from "./Tictactoe";
+
+export default function Ultimate() {
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td><Tictactoe /></td>
+            <td><Tictactoe /></td>
+            <td><Tictactoe /></td>
+          </tr>
+          <tr>
+            <td><Tictactoe /></td>
+            <td><Tictactoe /></td>
+            <td><Tictactoe /></td>
+          </tr>
+          <tr>
+            <td><Tictactoe /></td>
+            <td><Tictactoe /></td>
+            <td><Tictactoe /></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
