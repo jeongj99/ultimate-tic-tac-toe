8daf07f29b6b91e8a856b8b3906173922515e7d2
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-06 20:03:21 -0300

    Remove ultimate from App and just work on Tictactoe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Ultimate from './components/Ultimate';
+import Tictactoe from './components/Tictactoe';
 
 import './App.css';
 
 function App() {
   return (
-    <Ultimate />
+    <Tictactoe />
   );
 }
 
