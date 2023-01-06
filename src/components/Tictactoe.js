6942a110827e4cc638dea74533e8ae51a003c28a
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-05 21:10:52 -0300

    Add onClick to all the tds in Tictactoe component

--- src/components/Tictactoe.js
+++ src/components/Tictactoe.js
@@ -1,23 +1,25 @@
 import "./Tictactoe.css";
 
-export default function Tictactoe() {
+export default function Tictactoe(props) {
+  const { handleClick } = props;
+
   return (
     <table className="individual-board">
       <tbody>
         <tr>
-          <td className="individual"></td>
-          <td className="individual"></td>
-          <td className="individual"></td>
+          <td className="individual" onClick={handleClick}></td>
+          <td className="individual" onClick={handleClick}></td>
+          <td className="individual" onClick={handleClick}></td>
         </tr>
         <tr>
-          <td className="individual"></td>
-          <td className="individual"></td>
-          <td className="individual"></td>
+          <td className="individual" onClick={handleClick}></td>
+          <td className="individual" onClick={handleClick}></td>
+          <td className="individual" onClick={handleClick}></td>
         </tr>
         <tr>
-          <td className="individual"></td>
-          <td className="individual"></td>
-          <td className="individual"></td>
+          <td className="individual" onClick={handleClick}></td>
+          <td className="individual" onClick={handleClick}></td>
+          <td className="individual" onClick={handleClick}></td>
         </tr>
       </tbody>
     </table>
