16ce4d049f748420beebca5100f1244172d6b579
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-05 21:08:38 -0300

    Create a turnState, a handleClick function and pass it to Tictactoe as prop

--- src/components/Ultimate.js
+++ src/components/Ultimate.js
@@ -5,26 +5,35 @@ import Tictactoe from "./Tictactoe";
 import "./Ultimate.css";
 
 export default function Ultimate() {
+  const [turn, setTurn] = useState("x");
   const [activeUltimateCell, setActiveUltimateCell] = useState("all");
+
+  const handleClick = () => {
+    if (turn === "x") {
+      setTurn("o");
+    } else {
+      setTurn("x");
+    }
+  };
 
   return (
     <div>
       <table className="ultimate-board">
         <tbody>
           <tr >
-            <td className="ultimate-cell"><Tictactoe /></td>
-            <td className="ultimate-cell"><Tictactoe /></td>
-            <td className="ultimate-cell"><Tictactoe /></td>
+            <td className="ultimate-cell"><Tictactoe handleClick={handleClick} /></td>
+            <td className="ultimate-cell"><Tictactoe handleClick={handleClick} /></td>
+            <td className="ultimate-cell"><Tictactoe handleClick={handleClick} /></td>
           </tr>
           <tr>
-            <td className="ultimate-cell"><Tictactoe /></td>
-            <td className="ultimate-cell"><Tictactoe /></td>
-            <td className="ultimate-cell"><Tictactoe /></td>
+            <td className="ultimate-cell"><Tictactoe handleClick={handleClick} /></td>
+            <td className="ultimate-cell"><Tictactoe handleClick={handleClick} /></td>
+            <td className="ultimate-cell"><Tictactoe handleClick={handleClick} /></td>
           </tr>
           <tr>
-            <td className="ultimate-cell"><Tictactoe /></td>
-            <td className="ultimate-cell"><Tictactoe /></td>
-            <td className="ultimate-cell"><Tictactoe /></td>
+            <td className="ultimate-cell"><Tictactoe handleClick={handleClick} /></td>
+            <td className="ultimate-cell"><Tictactoe handleClick={handleClick} /></td>
+            <td className="ultimate-cell"><Tictactoe handleClick={handleClick} /></td>
           </tr>
         </tbody>
       </table>
