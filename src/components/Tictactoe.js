58bf252b4c3288fa5e6d16bdd038e14778d70239
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-06 21:48:42 -0300

    Fix handleClick bug and pass it as onClick prop for Cell component

--- src/components/Tictactoe.js
+++ src/components/Tictactoe.js
@@ -17,15 +17,17 @@ export default function Tictactoe(props) {
   const handleClick = (index) => {
     const squares = [...cells];
 
-    squares[index] = turn;
+    if (!squares[index]) {
+      squares[index] = turn;
 
-    if (turn === "x") {
-      setTurn("o");
-    } else {
-      setTurn("x");
+      if (turn === "x") {
+        setTurn("o");
+      } else {
+        setTurn("x");
+      }
+
+      setCells(squares);
     }
-
-    setCells(squares);
   };
 
   return (
@@ -33,35 +35,35 @@ export default function Tictactoe(props) {
       <tbody>
         <tr>
           <td className="individual">
-            <Cell value={cells[0]} />
+            <Cell value={cells[0]} onClick={() => handleClick(0)} />
           </td>
           <td className="individual">
-            <Cell value={cells[1]} />
+            <Cell value={cells[1]} onClick={() => handleClick(1)} />
           </td>
           <td className="individual">
-            <Cell value={cells[2]} />
-          </td>
-        </tr>
-        <tr>
-          <td className="individual">
-            <Cell value={cells[3]} />
-          </td>
-          <td className="individual">
-            <Cell value={cells[4]} />
-          </td>
-          <td className="individual">
-            <Cell value={cells[5]} />
+            <Cell value={cells[2]} onClick={() => handleClick(2)} />
           </td>
         </tr>
         <tr>
           <td className="individual">
-            <Cell value={cells[6]} />
+            <Cell value={cells[3]} onClick={() => handleClick(3)} />
           </td>
           <td className="individual">
-            <Cell value={cells[7]} />
+            <Cell value={cells[4]} onClick={() => handleClick(4)} />
           </td>
           <td className="individual">
-            <Cell value={cells[8]} />
+            <Cell value={cells[5]} onClick={() => handleClick(5)} />
+          </td>
+        </tr>
+        <tr>
+          <td className="individual">
+            <Cell value={cells[6]} onClick={() => handleClick(6)} />
+          </td>
+          <td className="individual">
+            <Cell value={cells[7]} onClick={() => handleClick(7)} />
+          </td>
+          <td className="individual">
+            <Cell value={cells[8]} onClick={() => handleClick(8)} />
           </td>
         </tr>
       </tbody>
