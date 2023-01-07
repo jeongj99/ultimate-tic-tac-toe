6d912493d114b3c16f189f3f18bcacfb19556417
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-06 21:38:34 -0300

    Create Cell component function and update handleClick function

--- src/components/Tictactoe.js
+++ src/components/Tictactoe.js
@@ -2,34 +2,67 @@ import { useState } from "react";
 
 import "./Tictactoe.css";
 
+function Cell({ value, onClick }) {
+  return (
+    <button onClick={onClick}>
+      {value}
+    </button>
+  );
+}
+
 export default function Tictactoe(props) {
   const [turn, setTurn] = useState("x");
+  const [cells, setCells] = useState(Array(9).fill(null));
 
-  const handleClick = () => {
+  const handleClick = (index) => {
+    const squares = [...cells];
+
+    squares[index] = turn;
+
     if (turn === "x") {
       setTurn("o");
     } else {
       setTurn("x");
     }
+
+    setCells(squares);
   };
 
   return (
     <table className="individual-board">
       <tbody>
         <tr>
-          <td className="individual" onClick={handleClick}></td>
-          <td className="individual" onClick={handleClick}></td>
-          <td className="individual" onClick={handleClick}></td>
+          <td className="individual" onClick={handleClick}>
+            <Cell />
+          </td>
+          <td className="individual" onClick={handleClick}>
+            <Cell />
+          </td>
+          <td className="individual" onClick={handleClick}>
+            <Cell />
+          </td>
         </tr>
         <tr>
-          <td className="individual" onClick={handleClick}></td>
-          <td className="individual" onClick={handleClick}></td>
-          <td className="individual" onClick={handleClick}></td>
+          <td className="individual" onClick={handleClick}>
+            <Cell />
+          </td>
+          <td className="individual" onClick={handleClick}>
+            <Cell />
+          </td>
+          <td className="individual" onClick={handleClick}>
+            <Cell />
+          </td>
         </tr>
         <tr>
-          <td className="individual" onClick={handleClick}></td>
-          <td className="individual" onClick={handleClick}></td>
-          <td className="individual" onClick={handleClick}></td>
+          <td className="individual" onClick={handleClick}>
+            <Cell />
+          </td>
+          <td className="individual" onClick={handleClick}>
+            <Cell />
+          </td>
+          <td className="individual" onClick={handleClick}>
+            <Cell />
+          </td>
         </tr>
       </tbody>
     </table>
