2065e7611bc66384b0f9b15410f7d80c49efc2d3
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-09 21:50:09 -0300

    Import Tictactoe to Ultimate component and display it

--- src/components/Ultimate.js
+++ src/components/Ultimate.js
@@ -1,30 +1,44 @@
-import { useState } from "react";
-
 import Tictactoe from "./Tictactoe";
 
 import "./Ultimate.css";
 
 export default function Ultimate() {
-  const [activeUltimateCell, setActiveUltimateCell] = useState("all");
-
   return (
     <div>
       <table className="ultimate-board">
         <tbody>
           <tr >
-            <td className="ultimate-cell"></td>
-            <td className="ultimate-cell"></td>
-            <td className="ultimate-cell"></td>
+            <td className="ultimate-cell">
+              <Tictactoe />
+            </td>
+            <td className="ultimate-cell">
+              <Tictactoe />
+            </td>
+            <td className="ultimate-cell">
+              <Tictactoe />
+            </td>
           </tr>
           <tr>
-            <td className="ultimate-cell"></td>
-            <td className="ultimate-cell"></td>
-            <td className="ultimate-cell"></td>
+            <td className="ultimate-cell">
+              <Tictactoe />
+            </td>
+            <td className="ultimate-cell">
+              <Tictactoe />
+            </td>
+            <td className="ultimate-cell">
+              <Tictactoe />
+            </td>
           </tr>
           <tr>
-            <td className="ultimate-cell"></td>
-            <td className="ultimate-cell"></td>
-            <td className="ultimate-cell"></td>
+            <td className="ultimate-cell">
+              <Tictactoe />
+            </td>
+            <td className="ultimate-cell">
+              <Tictactoe />
+            </td>
+            <td className="ultimate-cell">
+              <Tictactoe />
+            </td>
           </tr>
         </tbody>
       </table>
