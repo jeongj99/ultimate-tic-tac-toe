c3b8c38803b03e98075994c1f8ce98fefa575e90
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-09 21:54:46 -0300

    Return Ultimate in App.js and give borders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import Ultimate from './components/Ultimate';
-import Tictactoe from './components/Tictactoe';
 
 import './App.css';
 
 function App() {
   return (
-    <Tictactoe />
+    <Ultimate />
   );
 }
 
