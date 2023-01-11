14fe11d0accd25642507cd47acfeda233eee558f
Jae Yun Jeong <jeongalex0616@gmail.com>
2023-01-10 21:29:57 -0300

    Create ultimateCells state and pass it as prop to Tictactoe, and also pass id prop to Tictactoe

--- src/components/Ultimate.js
+++ src/components/Ultimate.js
@@ -6,6 +6,7 @@ import "./Ultimate.css";
 
 export default function Ultimate() {
   const [turn, setTurn] = useState("x");
+  const [ultimateCells, setUltimateCells] = useState(Array(9).fill(null));
 
   return (
     <div>
@@ -13,35 +14,35 @@ export default function Ultimate() {
         <tbody>
           <tr >
             <td className="ultimate-cell">
-              <Tictactoe turn={turn} setTurn={setTurn} />
+              <Tictactoe id={0} turn={turn} setTurn={setTurn} setUltimateCells={setUltimateCells} />
             </td>
             <td className="ultimate-cell">
-              <Tictactoe turn={turn} setTurn={setTurn} />
+              <Tictactoe id={1} turn={turn} setTurn={setTurn} setUltimateCells={setUltimateCells} />
             </td>
             <td className="ultimate-cell">
-              <Tictactoe turn={turn} setTurn={setTurn} />
-            </td>
-          </tr>
-          <tr>
-            <td className="ultimate-cell">
-              <Tictactoe turn={turn} setTurn={setTurn} />
-            </td>
-            <td className="ultimate-cell">
-              <Tictactoe turn={turn} setTurn={setTurn} />
-            </td>
-            <td className="ultimate-cell">
-              <Tictactoe turn={turn} setTurn={setTurn} />
+              <Tictactoe id={2} turn={turn} setTurn={setTurn} setUltimateCells={setUltimateCells} />
             </td>
           </tr>
           <tr>
             <td className="ultimate-cell">
-              <Tictactoe turn={turn} setTurn={setTurn} />
+              <Tictactoe id={3} turn={turn} setTurn={setTurn} setUltimateCells={setUltimateCells} />
             </td>
             <td className="ultimate-cell">
-              <Tictactoe turn={turn} setTurn={setTurn} />
+              <Tictactoe id={4} turn={turn} setTurn={setTurn} setUltimateCells={setUltimateCells} />
             </td>
             <td className="ultimate-cell">
-              <Tictactoe turn={turn} setTurn={setTurn} />
+              <Tictactoe id={5} turn={turn} setTurn={setTurn} setUltimateCells={setUltimateCells} />
+            </td>
+          </tr>
+          <tr>
+            <td className="ultimate-cell">
+              <Tictactoe id={6} turn={turn} setTurn={setTurn} setUltimateCells={setUltimateCells} />
+            </td>
+            <td className="ultimate-cell">
+              <Tictactoe id={7} turn={turn} setTurn={setTurn} setUltimateCells={setUltimateCells} />
+            </td>
+            <td className="ultimate-cell">
+              <Tictactoe id={8} turn={turn} setTurn={setTurn} setUltimateCells={setUltimateCells} />
             </td>
           </tr>
         </tbody>
